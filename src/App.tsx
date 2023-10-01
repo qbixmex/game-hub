ba0cc7693f615f21a81550641880d473a5824376
Daniel González Briseño <qbixmex@gmail.com>
2023-10-01 04:03:53 -0700

    chakra-ui-installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@chakra-ui/react';
+
 const App = () => {
   return (
     <main>
       <h1>Game Hub</h1>
+      <Button colorScheme="blue">Click</Button>
     </main>
   );
 };
