8d7d907bc2991aae3de95aeb0a414783cc8f7646
Daniel González Briseño <qbixmex@gmail.com>
2023-10-01 04:18:50 -0700

    built-responsive-layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { Button } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 const App = () => {
   return (
     <main>
-      <h1>Game Hub</h1>
-      <Button colorScheme="blue">Click</Button>
+      <Grid templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}>
+        <GridItem area="nav" bg="blue.600" color="white">
+          Navigation Goes Here
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold" color="gray.800">
+            Aside Goes Here
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="red.600" color="white">
+          Main Content Goes Here
+        </GridItem>
+      </Grid>
     </main>
   );
 };
