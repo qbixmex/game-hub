1d62a20fdb3d6668c343ccb6cef38cc175f658b2
Daniel González Briseño <qbixmex@gmail.com>
2023-10-04 17:53:10 -0700

    fixed-place-holder-image-game

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,8 +1,7 @@
-// TODO import noImage from '../assets/no-image-placeholder.webp';
+import noImage from '../assets/no-image-placeholder.webp';
 
 const getCroppedImageUrl = (url: string) => {
-  // TODO if (noImage) return;
-  if (!url) return;
+  if (!url) return noImage;
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + `crop/600/400/` + url.slice(index);
