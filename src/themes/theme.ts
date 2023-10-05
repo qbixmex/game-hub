398be9018c1161dcbd614bdea8e03b4dfec9d0da
Daniel González Briseño <qbixmex@gmail.com>
2023-10-04 21:40:11 -0700

    customized-theme-to-get-darker-grays

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -4,6 +4,22 @@ const config: ThemeConfig = {
   initialColorMode: 'system',
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: '#F9F9F9',
+      100: '#EDEDED',
+      200: '#D3D3D3',
+      300: '#B3B3B3',
+      400: '#A0A0A0',
+      500: '#898989',
+      600: '#6C6C6C',
+      700: '#202020',
+      800: '#121212',
+      900: '#111111',
+    }
+  }
+});
 
 export default theme;
